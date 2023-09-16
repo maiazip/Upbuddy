a622125c0e07d95d4ef055b36a3fe22bae2bbecd
Maia <matheusmaiabastos@hotmail.com>
2023-09-16 11:19:39 -0300

    Project First Commit

--- upload-ai-web/vite.config.ts
+++ upload-ai-web/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: ['@ffmpeg/ffmpeg', '@ffmpeg,util']
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
